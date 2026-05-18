a946dd74e21b2550a79fcf528cbb90e6b84baf01
lyanbrito <lyanbrito613@gmail.com>
2026-05-18 07:40:41 -0300

    feat: added api

--- src/pages/g5-pages/PetPage.jsx
+++ src/pages/g5-pages/PetPage.jsx
@@ -3,14 +3,19 @@ import PrimaryButton from "../../components/g5/PrimaryButton.jsx";
 import Header from "../../components/g5/Header.jsx";
 import Image from "../../assets/images/g5-placeholder-img.png";
 import * as S from "./styles.js";
-
+import {useDonation} from "../../helpers/g5/donationApi.js";
 
 export default function PetPage() {
+    const {pet, loading, err} = useDonation(1)
+    if (loading) return <p>Carregando</p>
+    if (err) return <p>Erro</p>;
+    // aqui eu pego de outra pãgina sla como
     return (
         <S.PageContainer>
             <Header/>
             <S.PageContent>
-                <S.PetName>Nome do Pet</S.PetName>
+                {/*aguardando documentação do grupo g2*/}
+                <S.PetName>{pet.name}</S.PetName>
                 <S.ImageWrapper>
                     <S.PetImage src={Image} alt="img-1"/>
                     <S.PetImage src={Image} alt="img-2"/>
@@ -18,14 +23,15 @@ export default function PetPage() {
                     <S.PetImage src={Image} alt="img-4"/>
                 </S.ImageWrapper>
                 <S.PetInfo>
-                    <p>location</p>
-                    <p>description</p>
+                    <p>{pet.description}</p>
                     <S.PetInfoTitle>Informações</S.PetInfoTitle>
-                    <p>species</p>
-                    <p>breed</p>
-                    <p>age(mouths)</p>
-                    <p>weight</p>
-                    <p>gender</p>
+                    <p>{pet.species}</p>
+                    <p>{pet.breed}</p>
+                    <p>{pet.ageMouths}</p>
+                    <p>{pet.gender}</p>
+                    <p>{pet.size}</p>
+                    <p>{pet.weight}</p>
+                    <p>{pet.cep}</p>
                 </S.PetInfo>
                 <PrimaryButton label="Converse com o doador" icon={<ConversationIcon/>} ref="/"/>
             </S.PageContent>
