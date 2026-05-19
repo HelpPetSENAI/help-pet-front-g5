7caf3d04b1dabc480aede9f690a10658837b46a8
lyanbrito <lyanbrito613@gmail.com>
2026-05-18 23:03:34 -0300

    fix: added bearer token

--- src/helpers/g5/donationApi.js
+++ src/helpers/g5/donationApi.js
@@ -1,31 +1,53 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 export const useDonation = (id) => {
-    const [pet, setPet] = useState([]);
+    const [pet, setPet] = useState(null);
+    const token = import.meta.env.VITE_API_TOKEN;
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [err, setErr] = useState(null);
 
     useEffect(() => {
-        const fetchDonation = async () => {
-            try {
-                const res = await axios.get(
-                    `https://help-pet-back-g2.azurewebsites.net/donations/view/${id}`
-                );
-
-                setPet(res.data.results);
-            } catch (err) {
-                console.error(err);
-                setError(err);
-            } finally {
+        axios.get(
+            `https://help-pet-back-g2.azurewebsites.net/donations/view/${id}`,
+            {headers: {'Authorization': `Bearer ${token}`}}
+        )
+            .then(response => {
+                setPet(response.data.results);
+            })
+            .catch(error => {
+                setErr(error);
+            })
+            .finally(() => {
                 setLoading(false);
-            }
-        };
-
-        if (id) {
-            fetchDonation();
-        }
+            });
     }, [id]);
 
-    return { pet, loading, error };
+    return {pet, loading, err}
+};
+
+export const useAllDonations = () => {
+    const token = import.meta.env.VITE_API_TOKEN;
+    const [donations, setDonations] = useState([]);
+
+    useEffect(() => {
+
+        axios.get(
+            "https://help-pet-back-g2.azurewebsites.net/donations/viewAll",
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+        )
+            .then(response => {
+                setDonations(response.data);
+            })
+            .catch(error => {
+                console.error("Erro ao buscar doações:", error);
+            });
+
+    }, []);
+
+    return donations;
 };
